1fe6e72d2073a98337de0060001dd1bfc65a5f8e
Emanuelsantanajean <107273204+Emanuelsantanajean@users.noreply.github.com>
2022-08-18 21:43:19 -0400

    Add files via upload

---
+++ sumar.cuadro/sumadera1.2/sumadera1.2/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace sumadera1._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.txt_num7 = new System.Windows.Forms.TextBox();
+            this.txt_num8 = new System.Windows.Forms.TextBox();
+            this.txt_resultado6 = new System.Windows.Forms.TextBox();
+            this.txt_num9 = new System.Windows.Forms.TextBox();
+            this.txt_num15 = new System.Windows.Forms.TextBox();
+            this.txt_num11 = new System.Windows.Forms.TextBox();
+            this.txt_num14 = new System.Windows.Forms.TextBox();
+            this.txt_num6 = new System.Windows.Forms.TextBox();
+            this.txt_num13 = new System.Windows.Forms.TextBox();
+            this.txt_num5 = new System.Windows.Forms.TextBox();
+            this.txt_num10 = new System.Windows.Forms.TextBox();
+            this.txt_num12 = new System.Windows.Forms.TextBox();
+            this.txt_num4 = new System.Windows.Forms.TextBox();
+            this.txt_num3 = new System.Windows.Forms.TextBox();
+            this.txt_resultado5 = new System.Windows.Forms.TextBox();
+            this.txt_resultado4 = new System.Windows.Forms.TextBox();
+            this.txt_resultado8 = new System.Windows.Forms.TextBox();
+            this.txt_resultado7 = new System.Windows.Forms.TextBox();
+            this.txt_resultado2 = new System.Windows.Forms.TextBox();
+            this.txt_resultado1 = new System.Windows.Forms.TextBox();
+            this.txt_resultado3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txt_num1.Location = new System.Drawing.Point(29, 35);
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(100, 20);
+            this.txt_num1.TabIndex = 0;
+            this.txt_num1.Text = "0";
+            this.txt_num1.TextChanged += new System.EventHandler(this.txt_num1_TextChanged);
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.Location = new System.Drawing.Point(29, 61);
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(100, 20);
+            this.txt_num2.TabIndex = 1;
+            this.txt_num2.Text = "0";
+            this.txt_num2.TextChanged += new System.EventHandler(this.txt_num2_TextChanged);
+            // 
+            // txt_num7
+            // 
+            this.txt_num7.Location = new System.Drawing.Point(135, 61);
+            this.txt_num7.Name = "txt_num7";
+            this.txt_num7.Size = new System.Drawing.Size(100, 20);
+            this.txt_num7.TabIndex = 3;
+            this.txt_num7.Text = "0";
+            this.txt_num7.TextChanged += new System.EventHandler(this.txt_num7_TextChanged);
+            // 
+            // txt_num8
+            // 
+            this.txt_num8.Location = new System.Drawing.Point(135, 87);
+            this.txt_num8.Name = "txt_num8";
+            this.txt_num8.Size = new System.Drawing.Size(100, 20);
+            this.txt_num8.TabIndex = 2;
+            this.txt_num8.Text = "0";
+            this.txt_num8.TextChanged += new System.EventHandler(this.txt_num8_TextChanged);
+            // 
+            // txt_resultado6
+            // 
+            this.txt_resultado6.Location = new System.Drawing.Point(347, 87);
+            this.txt_resultado6.Name = "txt_resultado6";
+            this.txt_resultado6.ReadOnly = true;
+            this.txt_resultado6.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado6.TabIndex = 5;
+            this.txt_resultado6.Text = "0";
+            // 
+            // txt_num9
+            // 
+            this.txt_num9.Location = new System.Drawing.Point(135, 113);
+            this.txt_num9.Name = "txt_num9";
+            this.txt_num9.Size = new System.Drawing.Size(100, 20);
+            this.txt_num9.TabIndex = 4;
+            this.txt_num9.Text = "0";
+            this.txt_num9.TextChanged += new System.EventHandler(this.txt_num9_TextChanged);
+            // 
+            // txt_num15
+            // 
+            this.txt_num15.Location = new System.Drawing.Point(241, 139);
+            this.txt_num15.Name = "txt_num15";
+            this.txt_num15.Size = new System.Drawing.Size(100, 20);
+            this.txt_num15.TabIndex = 7;
+            this.txt_num15.Text = "0";
+            this.txt_num15.TextChanged += new System.EventHandler(this.txt_num15_TextChanged);
+            // 
+            // txt_num11
+            // 
+            this.txt_num11.Location = new System.Drawing.Point(241, 35);
+            this.txt_num11.Name = "txt_num11";
+            this.txt_num11.Size = new System.Drawing.Size(100, 20);
+            this.txt_num11.TabIndex = 6;
+            this.txt_num11.Text = "0";
+            this.txt_num11.TextChanged += new System.EventHandler(this.txt_num11_TextChanged);
+            // 
+            // txt_num14
+            // 
+            this.txt_num14.Location = new System.Drawing.Point(241, 113);
+            this.txt_num14.Name = "txt_num14";
+            this.txt_num14.Size = new System.Drawing.Size(100, 20);
+            this.txt_num14.TabIndex = 9;
+            this.txt_num14.Text = "0";
+            this.txt_num14.TextChanged += new System.EventHandler(this.txt_num14_TextChanged);
+            // 
+            // txt_num6
+            // 
+            this.txt_num6.Location = new System.Drawing.Point(135, 35);
+            this.txt_num6.Name = "txt_num6";
+            this.txt_num6.Size = new System.Drawing.Size(100, 20);
+            this.txt_num6.TabIndex = 8;
+            this.txt_num6.Text = "0";
+            this.txt_num6.TextChanged += new System.EventHandler(this.txt_num6_TextChanged);
+            // 
+            // txt_num13
+            // 
+            this.txt_num13.Location = new System.Drawing.Point(241, 87);
+            this.txt_num13.Name = "txt_num13";
+            this.txt_num13.Size = new System.Drawing.Size(100, 20);
+            this.txt_num13.TabIndex = 11;
+            this.txt_num13.Text = "0";
+            this.txt_num13.TextChanged += new System.EventHandler(this.txt_num13_TextChanged);
+            // 
+            // txt_num5
+            // 
+            this.txt_num5.Location = new System.Drawing.Point(29, 139);
+            this.txt_num5.Name = "txt_num5";
+            this.txt_num5.Size = new System.Drawing.Size(100, 20);
+            this.txt_num5.TabIndex = 10;
+            this.txt_num5.Text = "0";
+            this.txt_num5.TextChanged += new System.EventHandler(this.txt_num5_TextChanged);
+            // 
+            // txt_num10
+            // 
+            this.txt_num10.Location = new System.Drawing.Point(135, 139);
+            this.txt_num10.Name = "txt_num10";
+            this.txt_num10.Size = new System.Drawing.Size(100, 20);
+            this.txt_num10.TabIndex = 13;
+            this.txt_num10.Text = "0";
+            this.txt_num10.TextChanged += new System.EventHandler(this.txt_num10_TextChanged);
+            // 
+            // txt_num12
+            // 
+            this.txt_num12.Location = new System.Drawing.Point(241, 61);
+            this.txt_num12.Name = "txt_num12";
+            this.txt_num12.Size = new System.Drawing.Size(100, 20);
+            this.txt_num12.TabIndex = 12;
+            this.txt_num12.Text = "0";
+            this.txt_num12.TextChanged += new System.EventHandler(this.txt_num12_TextChanged);
+            // 
+            // txt_num4
+            // 
+            this.txt_num4.Location = new System.Drawing.Point(29, 113);
+            this.txt_num4.Name = "txt_num4";
+            this.txt_num4.Size = new System.Drawing.Size(100, 20);
+            this.txt_num4.TabIndex = 15;
+            this.txt_num4.Text = "0";
+            this.txt_num4.TextChanged += new System.EventHandler(this.txt_num4_TextChanged);
+            // 
+            // txt_num3
+            // 
+            this.txt_num3.Location = new System.Drawing.Point(29, 87);
+            this.txt_num3.Name = "txt_num3";
+            this.txt_num3.Size = new System.Drawing.Size(100, 20);
+            this.txt_num3.TabIndex = 14;
+            this.txt_num3.Text = "0";
+            this.txt_num3.TextChanged += new System.EventHandler(this.txt_num3_TextChanged);
+            // 
+            // txt_resultado5
+            // 
+            this.txt_resultado5.Location = new System.Drawing.Point(347, 61);
+            this.txt_resultado5.Name = "txt_resultado5";
+            this.txt_resultado5.ReadOnly = true;
+            this.txt_resultado5.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado5.TabIndex = 17;
+            this.txt_resultado5.Text = "0";
+            // 
+            // txt_resultado4
+            // 
+            this.txt_resultado4.Location = new System.Drawing.Point(347, 35);
+            this.txt_resultado4.Name = "txt_resultado4";
+            this.txt_resultado4.ReadOnly = true;
+            this.txt_resultado4.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado4.TabIndex = 16;
+            this.txt_resultado4.Text = "0";
+            // 
+            // txt_resultado8
+            // 
+            this.txt_resultado8.Location = new System.Drawing.Point(347, 139);
+            this.txt_resultado8.Name = "txt_resultado8";
+            this.txt_resultado8.ReadOnly = true;
+            this.txt_resultado8.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado8.TabIndex = 19;
+            this.txt_resultado8.Text = "0";
+            // 
+            // txt_resultado7
+            // 
+            this.txt_resultado7.Location = new System.Drawing.Point(347, 113);
+            this.txt_resultado7.Name = "txt_resultado7";
+            this.txt_resultado7.ReadOnly = true;
+            this.txt_resultado7.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado7.TabIndex = 18;
+            this.txt_resultado7.Text = "0";
+            // 
+            // txt_resultado2
+            // 
+            this.txt_resultado2.Location = new System.Drawing.Point(135, 169);
+            this.txt_resultado2.Name = "txt_resultado2";
+            this.txt_resultado2.ReadOnly = true;
+            this.txt_resultado2.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado2.TabIndex = 21;
+            this.txt_resultado2.Text = "0";
+            // 
+            // txt_resultado1
+            // 
+            this.txt_resultado1.Location = new System.Drawing.Point(29, 169);
+            this.txt_resultado1.Name = "txt_resultado1";
+            this.txt_resultado1.ReadOnly = true;
+            this.txt_resultado1.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado1.TabIndex = 20;
+            this.txt_resultado1.Text = "0";
+            // 
+            // txt_resultado3
+            // 
+            this.txt_resultado3.Location = new System.Drawing.Point(241, 169);
+            this.txt_resultado3.Name = "txt_resultado3";
+            this.txt_resultado3.ReadOnly = true;
+            this.txt_resultado3.Size = new System.Drawing.Size(100, 20);
+            this.txt_resultado3.TabIndex = 23;
+            this.txt_resultado3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sumadera1._2.Properties.Resources.descarga__40_;
+            this.ClientSize = new System.Drawing.Size(479, 330);
+            this.Controls.Add(this.txt_resultado3);
+            this.Controls.Add(this.txt_resultado2);
+            this.Controls.Add(this.txt_resultado1);
+            this.Controls.Add(this.txt_resultado8);
+            this.Controls.Add(this.txt_resultado7);
+            this.Controls.Add(this.txt_resultado5);
+            this.Controls.Add(this.txt_resultado4);
+            this.Controls.Add(this.txt_num4);
+            this.Controls.Add(this.txt_num3);
+            this.Controls.Add(this.txt_num10);
+            this.Controls.Add(this.txt_num12);
+            this.Controls.Add(this.txt_num13);
+            this.Controls.Add(this.txt_num5);
+            this.Controls.Add(this.txt_num14);
+            this.Controls.Add(this.txt_num6);
+            this.Controls.Add(this.txt_num15);
+            this.Controls.Add(this.txt_num11);
+            this.Controls.Add(this.txt_resultado6);
+            this.Controls.Add(this.txt_num9);
+            this.Controls.Add(this.txt_num7);
+            this.Controls.Add(this.txt_num8);
+            this.Controls.Add(this.txt_num2);
+            this.Controls.Add(this.txt_num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TextChanged += new System.EventHandler(this.suma);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.TextBox txt_num7;
+        private System.Windows.Forms.TextBox txt_num8;
+        private System.Windows.Forms.TextBox txt_resultado6;
+        private System.Windows.Forms.TextBox txt_num9;
+        private System.Windows.Forms.TextBox txt_num15;
+        private System.Windows.Forms.TextBox txt_num11;
+        private System.Windows.Forms.TextBox txt_num14;
+        private System.Windows.Forms.TextBox txt_num6;
+        private System.Windows.Forms.TextBox txt_num13;
+        private System.Windows.Forms.TextBox txt_num5;
+        private System.Windows.Forms.TextBox txt_num10;
+        private System.Windows.Forms.TextBox txt_num12;
+        private System.Windows.Forms.TextBox txt_num4;
+        private System.Windows.Forms.TextBox txt_num3;
+        private System.Windows.Forms.TextBox txt_resultado5;
+        private System.Windows.Forms.TextBox txt_resultado4;
+        private System.Windows.Forms.TextBox txt_resultado8;
+        private System.Windows.Forms.TextBox txt_resultado7;
+        private System.Windows.Forms.TextBox txt_resultado2;
+        private System.Windows.Forms.TextBox txt_resultado1;
+        private System.Windows.Forms.TextBox txt_resultado3;
+    }
+}
+
---
+++ sumar.cuadro/sumadera1.2/sumadera1.2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sumadera1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+        private void suma(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txt_num1_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado1.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse (txt_num2.Text) + int.Parse(txt_num3.Text) + int.Parse(txt_num4.Text) + int.Parse(txt_num5.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num2_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado1.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num2.Text) + int.Parse(txt_num3.Text) + int.Parse(txt_num4.Text) + int.Parse(txt_num5.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num3_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado1.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num2.Text) + int.Parse(txt_num3.Text) + int.Parse(txt_num4.Text) + int.Parse(txt_num5.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num4_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado1.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num2.Text) + int.Parse(txt_num3.Text) + int.Parse(txt_num4.Text) + int.Parse(txt_num5.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num5_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado1.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num2.Text) + int.Parse(txt_num3.Text) + int.Parse(txt_num4.Text) + int.Parse(txt_num5.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+
+        }
+
+        private void txt_num6_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado2.Text = Convert.ToString(int.Parse(txt_num6.Text) + int.Parse(txt_num7.Text) + int.Parse(txt_num8.Text) + int.Parse(txt_num9.Text) + int.Parse(txt_num10.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num7_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado2.Text = Convert.ToString(int.Parse(txt_num6.Text) + int.Parse(txt_num7.Text) + int.Parse(txt_num8.Text) + int.Parse(txt_num9.Text) + int.Parse(txt_num10.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num8_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado2.Text = Convert.ToString(int.Parse(txt_num6.Text) + int.Parse(txt_num7.Text) + int.Parse(txt_num8.Text) + int.Parse(txt_num9.Text) + int.Parse(txt_num10.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num9_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado2.Text = Convert.ToString(int.Parse(txt_num6.Text) + int.Parse(txt_num7.Text) + int.Parse(txt_num8.Text) + int.Parse(txt_num9.Text) + int.Parse(txt_num10.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num10_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado2.Text = Convert.ToString(int.Parse(txt_num6.Text) + int.Parse(txt_num7.Text) + int.Parse(txt_num8.Text) + int.Parse(txt_num9.Text) + int.Parse(txt_num10.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num11_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado3.Text = Convert.ToString(int.Parse(txt_num11.Text) + int.Parse(txt_num12.Text) + int.Parse(txt_num13.Text) + int.Parse(txt_num14.Text) + int.Parse(txt_num15.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num12_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado3.Text = Convert.ToString(int.Parse(txt_num11.Text) + int.Parse(txt_num12.Text) + int.Parse(txt_num13.Text) + int.Parse(txt_num14.Text) + int.Parse(txt_num15.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num13_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado3.Text = Convert.ToString(int.Parse(txt_num11.Text) + int.Parse(txt_num12.Text) + int.Parse(txt_num13.Text) + int.Parse(txt_num14.Text) + int.Parse(txt_num15.Text));
+            txt_resultado4.Text = Convert.ToString(int.Parse(txt_num1.Text) + int.Parse(txt_num6.Text) + int.Parse(txt_num11.Text));
+        }
+
+        private void txt_num14_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado3.Text = Convert.ToString(int.Parse(txt_num11.Text) + int.Parse(txt_num12.Text) + int.Parse(txt_num13.Text) + int.Parse(txt_num14.Text) + int.Parse(txt_num15.Text));
+        }
+
+        private void txt_num15_TextChanged(object sender, EventArgs e)
+        {
+            txt_resultado3.Text = Convert.ToString(int.Parse(txt_num11.Text) + int.Parse(txt_num12.Text) + int.Parse(txt_num13.Text) + int.Parse(txt_num14.Text) + int.Parse(txt_num15.Text));
+        }
+    }
+}
